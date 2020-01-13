06de914ed12697d1a7107496109ceb091a634f7f
reubengt <reubengeorgethomas@gmail.com>
2020-01-13 20:33:07 +0000

    add button and empty states on form

--- src/Form.js
+++ src/Form.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useState } from "react";
+const Form = () => {
+  const [morning, setMorning] = useState("");
+  const [night, setNight] = useState("");
 
-const Form = () =>{
-    return(
-        <form>
-            <h3>Are you a morning person?</h3>
-            <input type="radio" name="" value="" />
-
-        </form>
-    )
-}
+  const handleSubmit = () => {
+    //read the current values of radio buttons and calculate output?
+  };
+  return (
+    <form>
+      <h3>Are you a morning person?</h3>
+      <label for="morning">Morning</label>
+      <input id="morning" type="radio" name="morningornight" value={morning} />
+      <label for="night">Night</label>
+      <input id="night" type="radio" name="morningornight" value={night} />
+      <button onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+};
 
 export default Form;
