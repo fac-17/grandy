be41b023aac8310d5c0e75c15410e9a2d8c247ae
andy <andy.william.mcdonald@gmail.com>
2020-01-13 19:46:47 +0000

    yeah boi

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () =>{
+    return(
+        <form>
+            <h3>Are you a morning person?</h3>
+            <input type="radio" name="" value="" />
+
+        </form>
+    )
+}
+
+export default Form;
